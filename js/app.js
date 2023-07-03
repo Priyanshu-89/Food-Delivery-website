ad8851f587cb2f157607ff575f5708d50c38a8b5
priyanshu <priyanshukumari93933@gmail.com>
2023-07-03 21:15:04 +0530

    Food Delivery website

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// window.addEventListener("load", function() {
+//   var preloader = document.querySelector(".preloader");
+//   var content = document.querySelector(".content");
+
+//   // Show content and hide preloader
+//   preloader.style.display = "none";
+//   content.style.display = "block";
+// });
+
+window.onload=function(){
+  document.getElementById('preloader').style.display="none";
+  document.getElementById('content').style.display="block";
+}
